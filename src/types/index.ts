f0b888d7eb3bfe151c4ee4878e291ac8c6ab68e2
Brenno Souza <brenno.daniel.98@gmail.com>
2026-04-02 09:02:47 -0300

    feat: add TypeScript interfaces for TVShow, Season, Episode, and Watchlist models

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,30 @@
-export {};
+export interface BaseAsset {
+  '@assetType': string;
+  '@key': string;
+}
+
+export interface TVShow extends BaseAsset {
+  title: string;
+  description: string;
+  rating: string;
+}
+
+export interface Season extends BaseAsset {
+  number: number;
+  description: string;
+  tvShow: { '@key': string };
+}
+
+export interface Episode extends BaseAsset {
+  number: number;
+  title: string;
+  description: string;
+  duration: number;
+  season: { '@key': string };
+}
+
+export interface Watchlist extends BaseAsset {
+  name: string;
+  description: string;
+  user: { '@key': string };
+}
